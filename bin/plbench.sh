f1c748c7850791e53fef0f4f210324e37a2e65c7
Jose Zalacain <Jose Zalacain@DESKTOP-NK7F6C3>
2020-03-29 14:03:39 -0400

    Rename package to io.github.prolobjectlink

--- bin/plbench.sh
+++ bin/plbench.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.prolog.jiprolog.JiPrologBenchmarkRunner ${1+"$@"}
+java -classpath "$(dirname "$(pwd)")/lib/*" io.github.prolobjectlink.prolog.jiprolog.JiPrologBenchmarkRunner ${1+"$@"}
